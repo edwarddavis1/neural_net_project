902d765c4150f09174514dd735063f50d4611619
Ed <edwarddavis@hotmail.co.uk>
2020-11-21 22:55:38 +0000

    Written perceptron code

--- perceptron.py
+++ perceptron.py
@@ -9,7 +9,7 @@ train_data = pd.read_csv("perceptron_training_data.csv")
 train_data = train_data.drop(train_data.columns[0], axis=1)
 
 # Visualise Training data
-train_plot_data = train_data
+train_plot_data = train_data.copy()
 train_plot_data['y'] = train_plot_data["y"].astype(str)
 fig = px.scatter(train_plot_data, x='x_1', y='x_2', color='y')
 fig.show()
@@ -23,9 +23,37 @@ X = np.concatenate((x.T, ones))                 # Input feature space
 w = np.matrix(np.random.uniform(-1, 1, d + 1))  # Weights (row vector)
 f = np.dot(w, X)    # Initialised model
 
+
 # See initial model
-ggplot(aes('x_1', 'x_2', color='y'), data=train_data) +\
+ggplot(aes('x_1', 'x_2', color='y'), data=train_plot_data) +\
     geom_point() +\
     geom_abline(intercept=-w.item(2) / w.item(1), slope=-w.item(0) / w.item(1))
 
-# Train the model
+# Output vector
+y = train_data["y"].values
+
+# TRAIN THE MODEL -------------------------------------------------------------
+
+# Hyperparameters
+max_iter = 100    # Maximum iterations
+lr_0 = 10         # Initial learning rate
+
+# Training loop
+for i in range(max_iter):
+    # Set learning rate
+    lr = lr_0 / (i + 1)
+
+    # Loop over data points
+    for j in range(n):
+        # Calculate model for each data point with most recent weights
+        f = np.dot(w, X[:, j])
+        # Check optimisation condition
+        if f.item(0) * y[j] <= 0:
+            # Update weights when optimisation condition is not met
+            w = w + np.asmatrix(lr * y[j] * X[:, j]).T
+
+
+# Plot the results
+ggplot(aes('x_1', 'x_2', color='y'), data=train_plot_data) +\
+    geom_point() +\
+    geom_abline(intercept=-w.item(2) / w.item(1), slope=-w.item(0) / w.item(1))
