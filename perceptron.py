fecf68044ce8d23deb53b88bc405232b3d47fc03
Carys Harvey <carysmharvey@gmail.com>
2020-11-21 19:11:57 +0000

    Training data & initial model

--- perceptron.py
+++ perceptron.py
@@ -1,10 +1,7 @@
-import numpy as np
-from numpy import linalg as LA
 import pandas as pd
-from scipy.stats import multivariate_normal
-import matplotlib.pyplot as plt
 import plotly.express as px
-
+import numpy as np
+from ggplot import *
 
 # Read in the training data - two separate MVN distributions
 train_data = pd.read_csv("perceptron_training_data.csv")
@@ -15,3 +12,19 @@ train_plot_data = train_data
 train_plot_data['y'] = train_plot_data["y"].astype(str)
 fig = px.scatter(train_plot_data, x='x_1', y='x_2', color='y')
 fig.show()
+
+# Initialise model = sum(w_i*x_i)
+x = np.matrix(train_data.drop('y', axis = 1).values) # drop output column y
+n = x.shape[0] # number data points
+d = x.shape[1] # feautre space dimensions
+ones = np.asmatrix(np.ones(n)) # ones for bias terms
+X = np.concatenate((x.T,ones))
+w = np.matrix(np.random.uniform(-1,1,d+1)) # ROW vector with 3 weights for 2D feature space (2 for data points + one bias)
+# model
+f = np.dot(w,X)
+# see initial model
+ggplot(aes("x_1", "x_2", color = 'y'), data = train_data) +\
+    geom_point() +\
+    geom_abline(intercept = -w.item(2)/w.item(1), slope = -w.item(0) / w.item(1)) #  point = scatter, abline = straight line
+
+# train the model
