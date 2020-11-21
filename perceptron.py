89e0c20526717d1a7eb337fb1748a82ca10f0f8b
Will Davis <willdavis576@gmail.com>
2020-11-21 22:58:21 +0000

    first trained perceptron

--- perceptron.py
+++ perceptron.py
@@ -8,7 +8,7 @@ train_data = pd.read_csv("perceptron_training_data.csv")
 train_data = train_data.drop(train_data.columns[0], axis=1)
 
 # Visualise Training data
-train_plot_data = train_data
+train_plot_data = train_data.copy()
 train_plot_data['y'] = train_plot_data["y"].astype(str)
 fig = px.scatter(train_plot_data, x='x_1', y='x_2', color='y')
 fig.show()
@@ -23,8 +23,40 @@ w = np.matrix(np.random.uniform(-1,1,d+1)) # ROW vector with 3 weights for 2D fe
 # model
 f = np.dot(w,X)
 # see initial model
-ggplot(aes("x_1", "x_2", color = 'y'), data = train_data) +\
+
+ggplot(aes("x_1", "x_2", color = 'y'), data = train_plot_data) +\
     geom_point() +\
     geom_abline(intercept = -w.item(2)/w.item(1), slope = -w.item(0) / w.item(1)) #  point = scatter, abline = straight line
 
 # train the model
+y = train_data["y"].values
+# Hyperparamters
+max_iter = 100  # Maximum iterations
+lr_0 = 10       # Initial learning rate
+
+# Training Loop
+for i in range(max_iter):
+    # Set learning rate
+    lr = lr_0 / (i + 1)
+
+    # Loop over data points
+    for j in range(n):
+        # Calculate model with most recent weights
+        f = np.dot(w, X[:, j])
+        # Check optimisation condition
+        if f.item(0) * y[j] <= 0:
+            w = w + np.asmatrix(lr * y[j] * X[:, j]).T
+
+
+w
+
+ggplot(aes("x_1", "x_2", color = 'y'), data = train_plot_data) + geom_point() + geom_abline(intercept = -w.item(2)/w.item(1), slope = -w.item(0) / w.item(1))
+
+
+
+
+
+
+
+
+#
