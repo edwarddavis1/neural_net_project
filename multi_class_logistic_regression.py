39c02220e0c1d911b424a8ba340dbe4f1b49fd14
Ed <edwarddavis@hotmail.co.uk>
2020-11-22 01:27:47 +0000

    New script for multi-class logistic regression (not yet coded)

---
+++ multi_class_logistic_regression.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from ggplot import *
+
+
+def sigmoid(x):
+    """Sigmoid function"""
+    return 1 / (np.exp(-x) + 1)
+
+
+# Read in the training data - two separate MVN distributions
+train_data = pd.read_csv("perceptron_training_data.csv")
+train_data = train_data.drop(train_data.columns[0], axis=1)
+
+# # Add outlier to see how the model reacts
+# train_data = train_data.append(pd.DataFrame({
+#     "x_1": [50],
+#     "x_2": [2],
+#     "y": [-1]
+# }), ignore_index=True)
+
+# Visualise Training data
+train_plot_data = train_data.copy()
+train_plot_data['y'] = train_plot_data["y"].astype(str)
+fig = px.scatter(train_plot_data, x='x_1', y='x_2', color='y')
+fig.show()
+
+# Initialise the model
+x = np.matrix(train_data.drop('y', axis=1).values)
+n = x.shape[0]      # Number of data points
+d = x.shape[1]      # Input feature dimensions
+ones = np.asmatrix(np.ones(n))
+X = np.concatenate((x.T, ones))                 # Input feature space
+w = np.matrix(np.random.uniform(-1, 1, d + 1))  # Weights (row vector)
+f = np.dot(w, X)    # Initialised model
+
+
+# See initial model
+ggplot(aes('x_1', 'x_2', color='y'), data=train_plot_data) +\
+    geom_point() +\
+    geom_abline(intercept=-w.item(2) / w.item(1), slope=-w.item(0) / w.item(1))
+
+# Output vector
+y = train_data["y"].values
+
+# TRAIN THE MODEL -------------------------------------------------------------
+
+# Hyperparameters
+max_iter = 100    # Maximum iterations
+lr_0 = 10         # Initial learning rate
+
+# Training loop
+for i in range(max_iter):
+    # Set learning rate
+    lr = lr_0 / (i + 1)
+
+    # Loop over data points
+    for j in range(n):
+        # Calculate model for each data point with most recent weights
+        f = np.dot(w, X[:, j])
+        # Check optimisation condition
+        if sigmoid(f.item(0) * y[j]) <= 0.5:
+            # Update weights when optimisation condition is not met
+            w = w + np.asmatrix(lr * y[j] * X[:, j]).T
+
+
+# Plot the results
+ggplot(aes('x_1', 'x_2', color='y'), data=train_plot_data) +\
+    geom_point() +\
+    geom_abline(intercept=-w.item(2) / w.item(1), slope=-w.item(0) / w.item(1))
