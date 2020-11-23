32a652989e99d5a4282e8ce252e1f88af434e8fa
Ed <edwarddavis@hotmail.co.uk>
2020-11-23 23:57:02 +0000

    Progrmmed neural network, including feedforward, backprop, as well as a prediction function. Loss function doesn't seem to be working correctly.

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def sigmoid(x):
@@ -8,7 +10,7 @@ def sigmoid(x):
 
 def sigmoid_derivative(x):
     """Differentiated sigmoid"""
-    return 1 / (1 + np.exp(-x)) ^ 2
+    return sigmoid(x) * (1 - sigmoid(x))
 
 
 class NeuralNet:
@@ -22,10 +24,14 @@ class NeuralNet:
         self.x = x
         self.y = y
         # Weights b/w input and hidden layer
-        self.w1 = np.random.rand(self.input.shape[1], 4)
+        self.w1 = np.random.rand(self.x.shape[1], 4)
         # Weights b/w hidden layer and output layer
         self.w2 = np.random.rand(4, 1)
-        self.output = np.zeros(y.shape)
+        self.output = np.zeros(self.y.shape)
+
+        # Store the loss after each iteration
+        self.losses = []
+        self.iterations = 0
 
     def feedforward(self):
         """Forward propagation of the weights throug the network"""
@@ -37,12 +43,61 @@ class NeuralNet:
     def backprop(self):
         """Backpropagation algorithm to update the weights of the model"""
         # Calculate the gradient of the loss function (in this case using LS)
-        d_w2 = np.dot(self.layer1.T, (2 * (self.y - self.output) *
-                                      sigmoid_derivative(self.output)))
-        d_w1 = np.dot(self.x.T, (np.dot(2 * (self.y - self.output) *
-                                        sigmoid_derivative(self.output), self.w2.T) *
-                                 sigmoid_derivative(self.layer1)))
+        z2 = np.vectorize(sigmoid_derivative)(self.output)
+        d_w2 = self.layer1.T * 2 * np.multiply((self.y - self.output), z2)
+
+        z1 = np.vectorize(sigmoid_derivative)(self.layer1)
+        d_w1 = self.x.T * 2 * \
+            np.multiply(np.multiply((self.y - self.output), z2) *
+                        self.w2.T, z1)
 
         # Update weights
         self.w2 += d_w2
         self.w1 += d_w1
+
+    def train(self, max_iter):
+        """Train the network by forward- and backpropatating weights"""
+        for i in range(max_iter):
+            # Propagate weights forward
+            self.feedforward()
+            self.iterations += 1
+            self.losses.append(np.square(NN.y - NN.output).sum())
+            # Update the weights and backpropagate
+            self.backprop()
+        # Calculate the output for the most up-to-date weights
+        self.feedforward()
+
+    def plot_training(self):
+        """Plot the loss as a function of iterations"""
+        fig = plt.plot(np.linspace(1, self.iterations,
+                                   self.iterations), self.losses)
+        return fig
+
+    def predict(self, x):
+        """Predicts an output given some input x"""
+        # Hidden layer
+        layer1_prediction = sigmoid(np.dot(x, self.w1))
+        # Output layer
+        output_prediction = sigmoid(np.dot(layer1_prediction, self.w2))
+        return output_prediction
+
+
+# Read in training data
+# train_data = pd.read_csv("Audiobooks_data.csv")
+train_data = pd.DataFrame({
+    "x1": [0, 0, 1, 1],
+    "x2": [0, 1, 0, 1],
+    "x3": [1, 1, 1, 1],
+    "y": [0, 1, 1, 0]
+})
+
+inputs = np.matrix(train_data.drop("y", axis=1).values)
+y = np.matrix(train_data.y.values).T
+
+NN = NeuralNet(inputs, y)
+NN.train(max_iter=10)
+
+new_inputs = inputs[:-1, 1].T
+NN.predict(new_inputs)
+
+NN.plot_training()
